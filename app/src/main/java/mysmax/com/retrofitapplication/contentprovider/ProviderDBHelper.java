1071a944c6b75b4100f04bfdf9708279ec9131fa
ChethanRGowda <r.chethan@yahoo.co.in>
2019-07-05 20:03:33 +0530

    UI commit

--- app/src/main/java/mysmax/com/retrofitapplication/contentprovider/ProviderDBHelper.java
+++ app/src/main/java/mysmax/com/retrofitapplication/contentprovider/ProviderDBHelper.java
@@ -1,6 +1,33 @@
 package mysmax.com.retrofitapplication.contentprovider;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class ProviderDBHelper extends SQLiteOpenHelper {
+
+    static final String DATABASE_NAME = "College";
+    static final String STUDENTS_TABLE_NAME = "students";
+    static final int DATABASE_VERSION = 1;
+    static final String CREATE_DB_TABLE =
+            " CREATE TABLE " + STUDENTS_TABLE_NAME +
+                    " (_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    " name TEXT NOT NULL, " +
+                    " grade TEXT NOT NULL);";
+
+    public ProviderDBHelper(Context context) {
+        super(context, DATABASE_NAME, null,  DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_DB_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " +  STUDENTS_TABLE_NAME);
+        onCreate(db);
+    }
+
 }
